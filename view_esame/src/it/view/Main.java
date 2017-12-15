367c6e304173be68843125528c6b2c1dfd50f508
Admin\Giorgio <gmarcuccio72@gmail.com>
2017-12-15 11:48:06 +0100

    prova

--- view_esame/src/it/view/Main.java
+++ view_esame/src/it/view/Main.java
@@ -1,9 +1,8 @@
 package it.view;
 
-import it.view.CardLayout.PrincUtentereg;
-
 public class Main {
     public static void main(String[] args) {
-                new PrincUtentereg();
+        new PrincUtentereg();
+
     }
 }
